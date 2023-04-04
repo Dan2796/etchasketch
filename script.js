403f0042c27ee7c2c85f3537732b18d41d2e9d5b
Dan Snow <danjsnow@outlook.com>
2023-04-04 15:48:27 +0100

    Add slider to allow user to dynamically change grid size

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 const etchASketch = document.querySelector('.etchASketch');
+const slider = document.querySelector("#colInput");
 
-// ask user for col number and work out grid size from that so that it is always square
-const colNumber = 10;
-// update the css to make sure it wraps on the right number of columns
-etchASketch.style.gridTemplateColumns = `repeat(${colNumber}, 1fr)`;
-
-const boxNumber = colNumber ** 2; // as many rows as columns
-for (let i = 0; i < boxNumber; i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    etchASketch.appendChild(box);
-    // special rules to round the four corners
-    if (i === 0) {
-        box.setAttribute('id', 'topLeft');
-    }
-    if (i === colNumber - 1) {
-        box.setAttribute('id', 'topRight');
-    }
-    if (i === boxNumber - colNumber) {
-        box.setAttribute('id', 'bottomLeft');
-    }
-    if (i === boxNumber - 1) {
-        box.setAttribute('id', 'bottomRight');
+function spawnBoxes(colNumber) {
+    const boxNumber = colNumber ** 2; // as many rows as columns
+    for (let i = 0; i < boxNumber; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        etchASketch.appendChild(box);
+        // special rules to round the four corners
+        if (i === 0) {
+            box.setAttribute('id', 'topLeft');
+        }
+        if (i === colNumber - 1) {
+            box.setAttribute('id', 'topRight');
+        }
+        if (i === boxNumber - colNumber) {
+            box.setAttribute('id', 'bottomLeft');
+        }
+        if (i === boxNumber - 1) {
+            box.setAttribute('id', 'bottomRight');
+        }
     }
 }
+
+// note - take col number so that it is always equal rows and cols, otherwise would need to ask for number of boxes
+// then check it has a square root etc
+function drawGrid(colNumber) {
+    // remove all boxes
+    const allBoxes = document.querySelectorAll('.box');
+    allBoxes.forEach(box => {
+        box.remove();
+    });
+
+
+    spawnBoxes(colNumber);
+    // update the css to make sure it wraps on the right number of columns
+    etchASketch.style.gridTemplateColumns = `repeat(${colNumber}, 1fr)`;
+}
+
+// default grid draw at middle of scale
+drawGrid(30);
+
+slider.oninput = function() {
+    drawGrid(this.value);
+    etchASketch.style.gridTemplateColumns = `repeat(${this.value}, 1fr)`;
+}
+
 
