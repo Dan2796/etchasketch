fe8a95537220190044f3cf790affee1190894c96
Dan Snow <danjsnow@outlook.com>
2023-04-05 15:04:17 +0100

    Adds clear button and refactors code a bit
    I put all the functions at the bottom and the event listeners at the top of the page.

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 const etchASketch = document.querySelector('.etchASketch');
 const slider = document.querySelector("#colInput");
 const gridToggle = document.querySelector(".gridToggle");
+const clearButton = document.querySelector(".clearButton");
+
 let gridShown = false; // start with grid not shown
+let currentColNumber = 30; // default
+
+// draw default grid before user has done anything
+drawGrid(currentColNumber);
+
+// need to track mouse depress to see if user is trying to actually draw! Used in listenForClicksOnAnyBox()
+let mouseIsDown = false;
+addEventListener('mousedown', () => mouseIsDown = true);
+addEventListener('mouseup', () => mouseIsDown = false);
+
+slider.oninput = function() {
+    currentColNumber = this.value;
+    drawGrid(currentColNumber);
+}
+
+gridToggle.addEventListener('click', () => {
+    toggleGridLines(gridShown);
+    gridShown = !gridShown;
+});
+
+clearButton.addEventListener('click', () => drawGrid(currentColNumber));
+
+function removeCurrentBoxes() {
+    const allBoxes = document.querySelectorAll('.box');
+    allBoxes.forEach(box => {
+        box.remove();
+    });
+}
+
 function spawnBoxes(colNumber) {
+    // note - takes col number so that it is always equal rows and cols, otherwise would need to ask for number of boxes
+    // then check it has a square root etc
     const boxNumber = colNumber ** 2; // as many rows as columns
     for (let i = 0; i < boxNumber; i++) {
         const box = document.createElement('div');
@@ -23,32 +56,10 @@ function spawnBoxes(colNumber) {
             box.setAttribute('id', 'bottomRight');
         }
     }
-}
-
-// note - take col number so that it is always equal rows and cols, otherwise would need to ask for number of boxes
-// then check it has a square root etc
-function drawGrid(colNumber) {
-
-    // remove all existing boxes
-    const allBoxes = document.querySelectorAll('.box');
-    allBoxes.forEach(box => {
-        box.remove();
-    });
-
-
-    spawnBoxes(colNumber);
-    // update the css to make sure it wraps on the right number of columns
+    // Update the css to make sure it wraps on the right number of columns. Note that it's a grid layout:
     etchASketch.style.gridTemplateColumns = `repeat(${colNumber}, 1fr)`;
-    listenForClicksOnAnyBox();
 }
 
-
-slider.oninput = function() {
-    drawGrid(this.value);
-    etchASketch.style.gridTemplateColumns = `repeat(${this.value}, 1fr)`;
-}
-
-// remove all existing boxes
 function listenForClicksOnAnyBox() {
     const allBoxes = document.querySelectorAll('.box');
     allBoxes.forEach(box => {
@@ -58,10 +69,12 @@ function listenForClicksOnAnyBox() {
     });
 }
 
-// need to track mouse depress to see if user is trying to actually draw!
-let mouseIsDown = false;
-addEventListener('mousedown', () => mouseIsDown = true);
-addEventListener('mouseup', () => mouseIsDown = false);
+function drawGrid(colNumber) {
+    removeCurrentBoxes();
+    spawnBoxes(colNumber);
+    listenForClicksOnAnyBox();
+}
+
 
 function toggleGridLines(gridShown) {
     const allBoxes = document.querySelectorAll('.box');
@@ -73,10 +86,6 @@ function toggleGridLines(gridShown) {
         }
     })
 }
-gridToggle.addEventListener('click', () => {
-    toggleGridLines(gridShown);
-    gridShown = !gridShown;
-});
 
-// default grid draw at middle of scale
-drawGrid(30);
+
+
