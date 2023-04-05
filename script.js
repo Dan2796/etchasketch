d7df13c3d3f7f84bf98539dc394e95f3fb87d66e
Dan Snow <danjsnow@outlook.com>
2023-04-05 16:32:45 +0100

    Adds shading option
    Needed to refactor so all colours are specified as rgb - allows for a regex adjustment on shading.

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const etchASketch = document.querySelector('.etchASketch');
 const slider = document.querySelector('#colInput');
 const gridToggle = document.querySelector('#gridToggle');
 const rainbowMode = document.querySelector('#rainbowMode');
+const shadingMode = document.querySelector('#shadingMode');
 const eraserMode = document.querySelector('#eraserMode');
 const clearButton = document.querySelector('.clearButton');
 
 let gridShown = false; // start with grid not shown
 let currentColNumber = 30; // default
 let rainbowModeActive = false;
+let shadingModeActive = false;
 let eraserModeActive = false;
 
 // draw default grid before user has done anything
@@ -23,9 +25,9 @@ function listenForClicksOnAnyBox() {
     const allBoxes = document.querySelectorAll('.box');
     allBoxes.forEach(box => {
         box.addEventListener('mouseover', () => {
-            if (mouseIsDown) colourInBox(box);
+            if (mouseIsDown) colorInBox(box);
         })
-        box.addEventListener('click', () => colourInBox(box)); // check for a click as well
+        box.addEventListener('click', () => colorInBox(box)); // check for a click as well
     });
 }
 
@@ -39,8 +41,21 @@ gridToggle.addEventListener('click', () => {
     gridShown = !gridShown;
 });
 
+// note that rainbow mode unchecks and turns off shading mode, and vice versa:
 rainbowMode.addEventListener('click', () => {
     rainbowModeActive = !rainbowModeActive;
+    if (rainbowModeActive) {
+        shadingModeActive = false;
+        shadingMode.checked = false;
+    }
+})
+
+shadingMode.addEventListener('click', () => {
+    shadingModeActive = !shadingModeActive;
+    if (shadingModeActive) {
+        rainbowModeActive = false;
+        rainbowMode.checked = false;
+    }
 })
 
 eraserMode.addEventListener('click', () => {eraserModeActive = !eraserModeActive;
@@ -68,6 +83,7 @@ function spawnBoxes(colNumber) {
     for (let i = 0; i < boxNumber; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
+        box.style.backgroundColor = 'rgb(255, 255, 255)'; // set here as rgb so it can be retrieved by shading version
         if (gridShown) box.classList.add('grid');
         etchASketch.appendChild(box);
         // special rules to round the four corners
@@ -88,22 +104,33 @@ function spawnBoxes(colNumber) {
     etchASketch.style.gridTemplateColumns = `repeat(${colNumber}, 1fr)`;
 }
 
-function colourInBox(box) {
+function colorInBox(box) {
     // manipulate the css directly rather than use classes because want a rainbow option
     if (eraserModeActive) {
-        box.style.backgroundColor = 'white';
+        box.style.backgroundColor = 'rgb(255, 255, 255)';
     } else if (rainbowModeActive) {
-        box.style.backgroundColor = generateRandomColour();
+        box.style.backgroundColor = generateRandomColor();
+    } else if (shadingModeActive) {
+        box.style.backgroundColor = darkenColor(box.style.backgroundColor);
     } else {
-        box.style.backgroundColor = 'black';
+        box.style.backgroundColor = 'rgb(0, 0, 0)';
     }
 }
 
-function generateRandomColour() {
-    const red = Math.floor(Math.random() * 256) + 1;
-    const green = Math.floor(Math.random() * 256) + 1;
-    const blue = Math.floor(Math.random() * 256) + 1;
+function generateRandomColor() {
+    const red = Math.floor(Math.random() * 255) + 1;
+    const green = Math.floor(Math.random() * 255) + 1;
+    const blue = Math.floor(Math.random() * 255) + 1;
     return `rgb(${red}, ${green}, ${blue})`
+}
+
+function darkenColor(backgroundColor) {
+    const colorVec = backgroundColor.replace(/[^\d,]/g, '').split(',');
+    let newRed = colorVec[0] - 30;
+    let newGreen = colorVec[1] - 30;
+    let newBlue = colorVec[2] - 30;
+    return `rgb(${newRed}, ${newGreen}, ${newBlue})`
+
 }
 
 function toggleGridLines(gridShown) {
@@ -116,6 +143,3 @@ function toggleGridLines(gridShown) {
         }
     })
 }
-
-
-
